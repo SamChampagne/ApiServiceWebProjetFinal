e56c3a72824afcc92967c07fd07fc1a0a719916f
SamChampagne <samolichampagne@icloud.com>
2024-05-13 15:26:42 -0400

    errorlog

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3030;
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 app.use(express.json());
+const errorlog = require('./src/middleware/detectionErreur.middleware');
 const utilisateurRoute = require('./src/routes/utilisateur.route');
 const tacheRoute = require('./src/routes/tache.route');
 const soustacheRoute = require('./src/routes/soustache.route');
@@ -17,10 +18,11 @@ const swaggerOptions = {
 app.use(cors());
 app.use(express.json());
 
+
 // Utilisation des routeurs
-app.use('/api/utilisateur', utilisateurRoute);
-app.use('/api/tache', tacheRoute);
-app.use('/api/soustache', soustacheRoute);
+app.use('/api/utilisateur',errorlog, utilisateurRoute);
+app.use('/api/tache',errorlog, tacheRoute);
+app.use('/api/soustache',errorlog, soustacheRoute);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 app.listen(PORT, () => 
---
+++ src/middleware/detectionErreur.middleware.js
@@ -0,0 +1,6 @@
+const file = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+const errorFile = file.createWriteStream(path.join(__dirname,'../', 'error.log'), { flags: 'a' });
+const logger = morgan('combined', { stream: errorFile, skip: (req, res) => res.statusCode < 500 });
+module.exports = {logger};
